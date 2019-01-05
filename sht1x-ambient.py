6e632158206024118b957ca606c6f893766d86e6
keitanak <45114941+keitanak@users.noreply.github.com>
2019-01-05 18:29:51 +0900

    Create sht1x-ambient.py

---
+++ sht1x-ambient.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+# while True:
+#     SensorTagをスキャンし、見つけたデバイスに接続し、
+#     温度、湿度、気圧、照度、バッテリーレベルを取得し、Ambientに送信
+#
+import bluepy
+import time
+import sys
+import argparse
+import ambient
+import argparse
+import RPi.GPIO as GPIO
+from pi_sht1x import SHT1x
+
+import logging
+
+logger = logging.getLogger()
+logging.basicConfig(stream=sys.stdout, level=logging.ERROR)
+
+class Choices(list):
+    def __contains__(self, item):
+        return super(Choices, self).__contains__(item.upper())
+        
+def main():
+    channelId = チャネルID
+    writeKey = 'ライトキー'
+
+    vdd_choices = Choices(['5V', '4V', '3.5V', '3V', '2.5V'])
+    resolution_choices = Choices(['HIGH', 'LOW'])
+
+    parser = argparse.ArgumentParser(description='Reads the temperature and relative humidity from the SHT1x series '
+                                                 'of sensors using the pi_sht1x library.')
+    parser.add_argument('data_pin', type=int, metavar='data-pin', help='Data pin used to connect to the sensor.')
+    parser.add_argument('sck_pin', type=int, metavar='sck-pin', help='SCK pin used to connect to the sensor.')
+    parser.add_argument('-g', '--gpio-mode', choices=['BCM', 'BOARD'], default='BCM',
+                        help='RPi.GPIO mode used, either GPIO.BOARD or GPIO.BCM. Defaults to GPIO.BCM.')
+    parser.add_argument('-v', '--vdd', choices=vdd_choices, default='3.5V',
+                        help='Voltage used to power the sensor. Defaults to 3.5V.')
+    parser.add_argument('-r', '--resolution', choices=resolution_choices, default='HIGH',
+                        help='Resolution used by the sensor, 14/12-bit or 12-8-bit. Defaults to High.')
+    parser.add_argument('-e', '--heater', action='store_true',
+                        help='Used to turn the internal heater on (used for calibration).')
+    parser.add_argument('-o', '--otp-no-reload', action='store_true',
+                        help='Used to enable OTP no reload, will save about 10ms per measurement.')
+    parser.add_argument('-c', '--no-crc-check', action='store_false', help='Performs CRC checking.')
+    parser.add_argument('-d', '--debug', action='store_true', help='Enable debug logging.')
+    parser.add_argument('-i',action='store',type=float, default=120.0, help='scan interval')
+    parser.add_argument('-t',action='store',type=float, default=5.0, help='scan time out')
+
+    args = parser.parse_args()
+
+    am = ambient.Ambient(channelId, writeKey)
+
+    while True:
+        
+        if args.debug:
+            logger.setLevel(logging.DEBUG)
+        mode = GPIO.BCM if args.gpio_mode.upper() == 'BCM' else GPIO.BOARD
+        with SHT1x(args.data_pin, args.sck_pin, gpio_mode=mode, vdd=args.vdd, resolution=args.resolution,
+               heater=args.heater, otp_no_reload=args.otp_no_reload, crc_check=args.no_crc_check,
+               logger=logger) as sensor:
+        temp = sensor.read_temperature()
+        humidity = sensor.read_humidity(temp)
+            
+        if dev is not None:
+            data = {}
+            data['d1'] = float(temp)  # set ambient temperature to d1
+            data['d2'] = float(humidity)  # set humidity to d2
+            
+            print(data)
+            r = am.send(data)
+            print(r.status_code)
+            sys.stdout.flush()
+
+            time.sleep(arg.i)
+            # tag.waitForNotifications(arg.t)
+
+            tag.disconnect()
+            del tag
+
+if __name__ == "__main__":
+    main()
